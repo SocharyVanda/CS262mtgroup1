5da8da8e017249cd7b32d8c42f181da107b2e3e0
kimlongoeun <kimlongoeun@gmail.com>
2026-06-02 22:46:57 +0700

    Create Crud for Backend

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
+Route::get('/signup', function () {
+    return view('signup');
+});
+
+Route::get('/login', function () {
+    return view('login');
+});
+
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login',[UserController::class, 'login']);
+Route::post('/logout',[UserController::class, 'logout']);
+
+Route::get('/dashboard', function () { return view('dashboard'); });
