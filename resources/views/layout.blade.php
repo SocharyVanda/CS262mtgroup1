afa1e41eef8a0219f670e75eaaa5ee146a65122c
Taingkea <staingkea@gmail.com>
2026-06-04 16:43:41 +0700

    delete new on nav

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -122,7 +122,6 @@
     </div>
     <div class="hidden md:flex gap-md">
         <a class="text-primary dark:text-primary-fixed-dim border-b-2 border-primary dark:border-primary-fixed-dim pb-1 font-bold text-body-md font-body-md transition-all duration-200 active:scale-95 hover:bg-surface-container-low dark:hover:bg-surface-variant px-2 rounded-t-sm" href="{{ url('') }}">Home</a>
-        <a class="text-on-surface-variant dark:text-outline-variant hover:text-primary dark:hover:text-primary-fixed-dim transition-colors text-body-md font-body-md transition-all duration-200 active:scale-95 hover:bg-surface-container-low dark:hover:bg-surface-variant px-2 rounded-sm pb-1" href="#">News</a>
         <a class="text-on-surface-variant dark:text-outline-variant hover:text-primary dark:hover:text-primary-fixed-dim transition-colors text-body-md font-body-md transition-all duration-200 active:scale-95 hover:bg-surface-container-low dark:hover:bg-surface-variant px-2 rounded-sm pb-1" href="#">Bookmarks</a>
         <a class="nav-item nav-link link-body-emphasis" href="/signup">sign up</a>
     </div>
