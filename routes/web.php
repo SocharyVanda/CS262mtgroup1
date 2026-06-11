2ac6f48d3f523e1b62c273edda0acc7f2d1aa983
chary <yrahctea5678@gmail.com>
2026-06-11 16:40:20 +0700

    add postcontroller to route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
 use App\Http\Controllers\NewsController;
 
